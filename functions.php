9675debbade2c4ab3825356e21a38856db57e4ac
Moiseées Garcia <moises.garcia@envia.com>
2020-09-26 21:41:34 -0500

    chances

--- functions.php
+++ functions.php
@@ -3,7 +3,12 @@ include 'custom/hooks/storefront_header.php';
 
 	add_action( 'wp_enqueue_scripts', 'storefront_2_enqueue_styles' );
 	function storefront_2_enqueue_styles() {
- 		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+		 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+		 wp_enqueue_script('slick', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', null, null, true);
+		 wp_enqueue_style('slick-style', 'http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', null, null);
+		 wp_enqueue_style('slick-theme', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css', null, null);
+		 wp_enqueue_script('custom', get_stylesheet_directory_uri() . '/assets/custom.js', null, null, true);
+		 wp_enqueue_style('custom', get_stylesheet_directory_uri() . '/assets/custom.css', null, null);
 		// echo get_page_template();
 		// exit;
 	} 
